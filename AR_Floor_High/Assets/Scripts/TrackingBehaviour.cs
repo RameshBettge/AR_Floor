e521adef803b706cf22ade376852145ae9365064
LB31 <leonidb93@gmail.com>
2018-10-13 18:23:43 +0200

    add ar stuff

---
+++ AR_Floor_High/Assets/Scripts/TrackingBehaviour.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackingBehaviour : MonoBehaviour, ITrackableEventHandler
+{
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+
+    Vector3[] allMarkers = new Vector3[2];
+    Rigidbody rb;
+    bool move = false;
+    Vector3 resultDistance;
+    public GameObject trapPrefab;
+
+    protected virtual void Start() {
+        rb = transform.GetChild(0).GetComponent<Rigidbody>();
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy() {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+    TrackableBehaviour.Status previousStatus,
+    TrackableBehaviour.Status newStatus) {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        } else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                   newStatus == TrackableBehaviour.Status.NO_POSE) {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        } else {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    void FixedUpdate() {
+        if(move)
+        rb.AddForce(transform.forward * 10);
+    }
+
+    protected virtual void OnTrackingFound() {
+
+
+        // Stores position of the found marker
+        if (allMarkers.Length == 0)
+            allMarkers[0] = transform.position;
+        else
+            allMarkers[1] = transform.position;
+        if(allMarkers.Length == 2) {
+            resultDistance = allMarkers[1] - allMarkers[0];
+            Vector3.Normalize(resultDistance);
+            Instantiate(trapPrefab, resultDistance*2, transform.rotation);
+        }
+
+
+
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+    protected virtual void OnTrackingLost() {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+}
